9d5234b1810189541222698f0ec2c4e905094836
Heba <36724067+heba@users.noreply.github.com>
2020-08-16 16:33:27 +0300

    adds the age calculator function

--- age_calculator.py
+++ age_calculator.py
@@ -1,13 +1,30 @@
 from datetime import datetime
 
 def check_birthdate(year, month, day):
-	# write code here
+	today = datetime.today()
+	birthday = datetime(year, month, day)
+	diff = today - birthday
+	if (diff.days <= 0):
+		return False
+	else:
+		return True
 
 def calculate_age(year, month, day):
-    # write code here
+	today = datetime.today()
+	birthday = datetime(year, month, day)
+	age = today - birthday
+	print ("You are %s years old." %(int(age.days/365)))
+
 
 def main():
-	# write main code here
+	year = input("Enter year of birth: ")
+	month = input("Enter month of birth: ")
+	day = input("Enter day of birth: ")
+
+	if check_birthdate(int(year), int(month), int(day)) == True :
+		calculate_age(int(year), int(month), int(day))
+	else:
+		print("birthdate entered is invalid")
 
 
 if __name__ == '__main__':
